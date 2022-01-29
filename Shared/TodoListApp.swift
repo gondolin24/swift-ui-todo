4b56689779abd1e5caaf4f9e62b0e94026c626e4
Eduardo Guzman Diaz <eduardoguzmandiaz@Eduardos-Air.cgocable.net>
2022-01-28 23:23:18 -0500

    initial commit

---
+++ Shared/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  Shared
+//
+//  Created by Eduardo Guzman Diaz on 2022-01-28.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
